f81411f55ad12979c79b2bf2c074b83eaa5abd95
Jayan Zaman <jayanzaman@gmail.com>
2017-02-25 19:54:17 -0500

    celcius to fahrenheit

---
+++ python-projects/CtoF.py
@@ -0,0 +1,7 @@
+#Please create a function that converts Celsius degrees to Fahrenheit.
+
+def celciusToFahrenheit(celcius):
+    fahrenheit = celcius*1.8+32
+    print(fahrenheit)
+
+celciusToFahrenheit(100)
---
+++ python-projects/script.py
@@ -0,0 +1,4 @@
+# python has numbers, floats, complex numbers such as 56j
+print(7/2)
+# we can also import math in python
+#dir(math) gives us all the options with math function
